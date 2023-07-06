12358fd15ade680e09fcefcf19b8f7f7d947db18
Alfonsina <alfonsina.l@gmail.com>
2023-07-05 21:46:26 -0400

    feat(app): implement adapter and local storage repository to handle the tasks on the app component

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App'
+import { App } from './app/components/App'
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
