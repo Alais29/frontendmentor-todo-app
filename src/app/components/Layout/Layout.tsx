b3d964ab58663e34b9686484355f23d79c2049f4
Alfonsina <alfonsina.l@gmail.com>
2023-07-19 16:56:50 -0400

    feat(styles): add dark mode

--- src/app/components/Layout/Layout.tsx
+++ src/app/components/Layout/Layout.tsx
@@ -1,11 +1,17 @@
 import classnames from 'classnames/bind'
 import styles from './layout.module.scss'
+import useLocalStorage from 'use-local-storage'
 
 const cx = classnames.bind(styles)
 
-export const Layout = ({ children }: { children: React.ReactNode }) => {
+interface ILayout {
+  children: React.ReactNode
+}
+
+export const Layout = ({ children }: ILayout) => {
+  const [theme] = useLocalStorage<'dark' | 'light'>('theme', 'light')
   return (
-    <div className={cx('layout')}>
+    <div className={cx('layout')} data-theme={theme}>
       <div className={cx('layout__header')} />
       <div className={cx('layout__content')}>{children}</div>
     </div>
--- src/app/components/Title/Title.tsx
+++ src/app/components/Title/Title.tsx
@@ -1,18 +1,26 @@
 import classnames from 'classnames/bind'
 import styles from './title.module.scss'
 import Moon from '../../../assets/images/icon-moon.svg'
+import Sun from '../../../assets/images/icon-sun.svg'
+import useLocalStorage from 'use-local-storage'
 
 const cx = classnames.bind(styles)
 
 interface ITitle {
   title: string
+  handleTheme: () => void
 }
 
-export const Title = ({ title }: ITitle) => {
+export const Title = ({ title, handleTheme }: ITitle) => {
+  const [theme] = useLocalStorage<'dark' | 'light'>('theme', 'light')
   return (
     <div className={cx('title')}>
       <h1 className={cx('title__text')}>{title}</h1>
-      <img src={Moon} alt='Dark Mode' />
+      <img
+        src={theme === 'light' ? Moon : Sun}
+        alt='Dark Mode'
+        onClick={handleTheme}
+      />
     </div>
   )
 }
